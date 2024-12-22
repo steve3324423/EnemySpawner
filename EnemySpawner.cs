fa9f11047bb22dffdd930594b70b2953d14d5ddf
steve3324423 <64463182+steve3324423@users.noreply.github.com>
2024-12-22 19:30:37 +0300

    Add files via upload

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private Transform[] _directions;
     [SerializeField] private EnemyFactory _factory;
 
+    private Vector3[] _directions = { Vector3.right, Vector3.forward, Vector3.up };
     private float _timeForCoroutine = 2f;
     private WaitForSeconds _waitSeconds;
 
@@ -27,8 +27,7 @@ public class EnemySpawner : MonoBehaviour
         while(isCoroutineRun)
         {
             Enemy enemy = _factory.Create(_spawnPoints[Random.Range(0,_spawnPoints.Length)].position);
-            enemy.SetDirection(_directions[Random.Range(0, _directions.Length)].position);
-
+            enemy.SetDirection(_directions[Random.Range(0,_directions.Length)]);
             yield return _waitSeconds;
         }
     }
